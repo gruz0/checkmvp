f8867fc285fa2ebfef18e8f804a1bf3f24374b65
Alexander Kadyrov <gruz0.mail@gmail.com>
2024-11-19 04:21:17 +0400

    Make progress bar user-friendly
    Resolves #113

--- src/components/FetchingDataMessage.tsx
+++ src/components/FetchingDataMessage.tsx
@@ -1,7 +1,10 @@
 const FetchingDataMessage = () => (
-  <div className="mb-6 mt-4 flex items-center rounded border border-purple-200 bg-purple-50 p-2 md:text-lg">
-    <div className="mr-4 h-6 w-24 animate-pulse rounded bg-purple-300" />
-    <p>Hang tight! We&apos;re fetching your data...</p>
+  <div className="mb-6 mt-4 flex items-center rounded-lg border border-purple-200 bg-purple-50 p-4 shadow-md md:text-lg">
+    <div className="mr-4 size-6 animate-spin rounded-full border-4 border-purple-500 border-t-transparent" />
+    <p>
+      Hang tight! We&apos;re fetching your data... This make take up to 30
+      seconds...
+    </p>
   </div>
 )
 
