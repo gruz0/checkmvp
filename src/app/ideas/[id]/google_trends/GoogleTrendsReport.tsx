01fcdc69707e52fa30026871d10f95cab1971667
Alexander Kadyrov <gruz0.mail@gmail.com>
2025-02-19 12:21:31 +0400

    Add Google Trends Keywords

---
+++ src/app/ideas/[id]/google_trends/GoogleTrendsReport.tsx
@@ -0,0 +1,42 @@
+import React from 'react'
+import BackToTopButton from '@/components/BackToTopButton'
+import HorizontalLine from '@/components/HorizontalLine'
+import Paragraph from '@/components/Paragraph'
+import { NavBar } from '../components/NavBar'
+import SectionGoogleTrends from './SectionSuggestedKeywords'
+
+interface Props {
+  googleTrendsKeywords: Array<string>
+}
+
+export const GoogleTrendsReport = ({ googleTrendsKeywords }: Props) => (
+  <div className="p-4 md:p-6 lg:p-8">
+    <div className="flex flex-col md:flex-row">
+      <aside className="sticky top-4 hidden self-start rounded-lg bg-gray-100 p-2 shadow-lg md:block md:w-1/4 dark:bg-gray-900">
+        <NavBar />
+      </aside>
+
+      <div className="flex-1 md:pl-8">
+        <div className="mb-6">
+          <h1 className="text-2xl font-bold text-gray-700 md:text-3xl lg:text-4xl dark:text-gray-200">
+            📈 Google Trends Keywords
+          </h1>
+        </div>
+
+        <Paragraph>
+          In this section, we look at popular search terms related to your
+          product. Understanding which keywords are trending can inform your
+          marketing strategy and help you attract the right audience. It&apos;s
+          a practical way to connect your idea with what people are already
+          searching for.
+        </Paragraph>
+
+        <HorizontalLine />
+
+        <SectionGoogleTrends googleTrendsKeywords={googleTrendsKeywords} />
+      </div>
+    </div>
+
+    <BackToTopButton />
+  </div>
+)
---
+++ src/app/ideas/[id]/google_trends/SectionSuggestedKeywords.tsx
@@ -0,0 +1,60 @@
+'use client'
+
+import Link from 'next/link'
+import React, { useState } from 'react'
+import Paragraph from '@/components/Paragraph'
+import SectionContainer from '@/components/SectionContainer'
+import SectionHeader from '@/components/SectionHeader'
+import SectionWrapper from '@/components/SectionWrapper'
+
+interface SectionGoogleTrendsProps {
+  googleTrendsKeywords: string[]
+}
+
+const SectionGoogleTrends: React.FC<SectionGoogleTrendsProps> = ({
+  googleTrendsKeywords,
+}) => {
+  const [isExpanded, setIsExpanded] = useState<boolean>(true)
+
+  return (
+    <SectionWrapper id="google_trends">
+      <SectionHeader
+        title="Suggested Keywords to Analyze"
+        emoji="📈"
+        onClick={() => setIsExpanded(!isExpanded)}
+        isExpanded={isExpanded}
+        sectionId="section_google_trends"
+      />
+
+      {isExpanded && (
+        <div id="section_google_trends">
+          <SectionContainer>
+            <Paragraph>
+              These AI-suggested keywords are a starting point for exploring
+              market trends. Click any keyword to view its popularity on Google
+              Trends in a new tab. We recommend checking different time periods
+              like 1 week and 90 days. Be sure to conduct your own research as
+              well.
+            </Paragraph>
+
+            <div className="mb-4 flex flex-wrap gap-2 lg:mb-6">
+              {googleTrendsKeywords.map((keyword) => (
+                <Link
+                  href={`https://trends.google.com/trends/explore?date=today%201-m&q=${keyword}&hl=en`}
+                  key={keyword}
+                  className="inline-flex cursor-pointer items-center justify-center rounded-full border border-gray-300 bg-white px-4 py-2 shadow transition-all duration-200 hover:bg-gray-200 dark:bg-gray-900 dark:hover:bg-gray-800"
+                  target="_blank"
+                  rel="nofollow noopener noreferrer"
+                >
+                  {keyword}
+                </Link>
+              ))}
+            </div>
+          </SectionContainer>
+        </div>
+      )}
+    </SectionWrapper>
+  )
+}
+
+export default SectionGoogleTrends
